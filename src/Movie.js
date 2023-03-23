cb7a12fa2191d959ace4081f84fe88121657ea8a
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-03-23 13:54:59 -1000

    almost done

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 
-class Movies extends React.Component {
+class Movie extends React.Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={this.props.image} alt='movie poster' title='movie poster' />
       <Card.Body>
-        <Card.Title>{this.props.movies.title}</Card.Title>
+        <Card.Title>{this.props.title}</Card.Title>
         <Card.Text>
           {this.props.overview}
         </Card.Text>
@@ -18,4 +18,4 @@ class Movies extends React.Component {
   }
 }
 
-export default Movies;
+export default Movie;
